205331f1610b93be9f13d00f1a74483a7ee67e9a
Andrei <An.S-1@yandex.ru>
2024-02-10 11:55:50 +0300

    task 2 - добавил иправление в task2

--- epam1A/src/main/java/org/example/task/Task2.java
+++ epam1A/src/main/java/org/example/task/Task2.java
@@ -9,15 +9,30 @@ public class Task2 extends Task{
     @Override
     public void task() {
         System.out.println("2. Отобразить в окне консоли аргументы командной строки в обратном порядке.");
-        action();
+        action2();
     }
-    public void action() {
+    public void action1() {
         while (true) {
-            System.out.println("ведите аргумент или 0 для выхода:");
+            System.out.println("введите аргумент или 0 для выхода:");
             String line = ReadConsole.getString();
             if (line.equals("0")) break;
-            System.out.println(" обратном порядке:");
+            System.out.println("обратном порядке:");
             System.out.println(new StringBuilder().append(line).reverse());
+        }
+        System.out.println("Вышли из цикла.");
+    }
+
+    public void action2() {
+        while (true) {
+            System.out.println("введите аргумент или 0 для выхода:");
+            String line = ReadConsole.getString();
+            if (line.equals("0")) break;
+            System.out.println("обратном порядке:");
+            String[] array = line.split("");
+            for (int i = array.length; i > 0; i--) {
+                System.out.print(array[i - 1]);
+            }
+            System.out.println();
         }
         System.out.println("Вышли из цикла.");
     }
