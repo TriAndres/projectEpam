34da3e9cb348ae8521ddef453a2abef1395a3f83
Andrei <An.S-1@yandex.ru>
2024-02-10 14:00:39 +0300

    task - добавил решение task3

--- epam1A/src/main/java/org/example/console/ReadConsole.java
+++ epam1A/src/main/java/org/example/console/ReadConsole.java
@@ -7,4 +7,17 @@ public class ReadConsole {
     public static String getString() {
         return scanner.nextLine();
     }
+
+    public static Integer getInteger() {
+        int a = 0;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                a = scanner.nextInt();
+                break;
+            } else {
+                scanner.nextLine();
+            }
+        }
+        return a;
+    }
 }
