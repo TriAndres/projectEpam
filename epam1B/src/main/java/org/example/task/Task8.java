1c86525835af89046b9e2ee7b660f6e3dc196923
Andrei <An.S-1@yandex.ru>
2024-02-18 10:51:39 +0300

    Добавил решение задачи 8

--- epam1B/src/main/java/org/example/task/Task8.java
+++ epam1B/src/main/java/org/example/task/Task8.java
@@ -1,6 +1,6 @@
 package org.example.task;
 /*
-
+8. Числа в порядке убывания частоты встречаемости чисел.
  */
 public class Task8 extends Task{
 
@@ -10,6 +10,50 @@ public class Task8 extends Task{
 
     @Override
     public void task() {
-        System.out.println("");
+        System.out.println("8. Числа в порядке убывания частоты встречаемости чисел.");
+        actionSort();
+        actionShow(10);
+    }
+
+    private void actionSort() {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    private void actionShow(int lineLength) {
+        int[] a = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    count++;
+                }
+            }
+            a[i] = count;
+        }
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            boolean isFlag = true;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    isFlag = false;
+                }
+            }
+            if (isFlag) {
+                count++;
+                System.out.print(array[i] + "-" + a[i] + "\t");
+                if (count == lineLength) {
+                    System.out.println();
+                    count = 0;
+                }
+            }
+        }
     }
 }
